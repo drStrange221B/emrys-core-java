ef63e707b2b2c07a237d6fe238b576ac673a6a02
Rojaya Maharjan <rmaharjan@apple.com>
2020-03-23 00:25:29 -0400

    fineInputSteam and BufferedInputStream

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,27 @@
 package com.company;
 
+import com.company.input.stream.BufferStream;
+
+import java.io.File;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("For Core Java ");
+
+        // find the current path location of the present file.
+        System.out.println(new File("").getAbsolutePath());
+        System.out.println(new File(System.getProperty("user.dir"))+ "/src/resources");
+
+        //navigate folder in java by .getParent()or .getParentfile(). on new File(System.getProperty("user.dir")
+
+        System.out.println(new File(System.getProperty("user.dir")).getParentFile());
+        System.out.println(new File(System.getProperty("user.dir")).getParentFile().getParentFile());
+
+
+//        BufferStream.fileInputStream();
+//
+//        BufferStream.bufferedInputStream();
+
     }
 }
